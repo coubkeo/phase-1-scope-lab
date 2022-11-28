1b11772f653e372064399ac803806642f0ca5d14
CHRISTINA OUBKEO <COUBKEO@GMAIL.COM>
2022-11-27 17:54:15 -0700

    commit tree 3afb77a99ff73369b79c1ab2e5dfea9f99116477

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution in this file!
+var customerName = "bob"
+const leastFavoriteCustomer = "tim"
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase()
+}
+function setBestCustomer(){
+    bestCustomer = "not bob"
+}
+function overwriteBestCustomer(newCustomer){
+    bestCustomer = newCustomer
+}
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "tom"
+}
